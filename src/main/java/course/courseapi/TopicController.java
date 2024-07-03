ecaa8ad30d878f47d904ac5681ff997d40f95382
feisalmombo <feisalmombo29@gmail.com>
2024-07-03 15:41:14 +0300

    WIP

--- src/main/java/course/courseapi/TopicController.java
+++ src/main/java/course/courseapi/TopicController.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TopicController {
     
+    public String getAllTopics() {
+        return "All Topics";
+    }
 }
