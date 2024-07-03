93ea38a764dedff78942084d42c8d47f646a5456
feisalmombo <feisalmombo29@gmail.com>
2024-07-03 15:54:36 +0300

    WIP

--- src/main/java/course/courseapi/TopicController.java
+++ src/main/java/course/courseapi/TopicController.java
@@ -1,10 +1,12 @@
 package course.courseapi;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-    
+
+    @RequestMapping("/topics")
     public String getAllTopics() {
         return "All Topics";
     }
