3781bb576daae964d2fb2c9b7983b0488bc4d529
feisalmombo <feisalmombo29@gmail.com>
2024-07-06 13:47:19 +0300

    WIP

--- src/main/java/course/courseapi/TopicController.java
+++ src/main/java/course/courseapi/TopicController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class TopicController {
     }
 
     @RequestMapping(method=RequestMethod.POST, value = "/topics")
-    public void addTopic(){
-
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
     }
 }
