2307b894d0c2a3adebc389d777c9c50bcbb22a7a
feisalmombo <feisalmombo29@gmail.com>
2024-07-08 12:26:30 +0300

    WIP

--- src/main/java/course/courseapi/TopicController.java
+++ src/main/java/course/courseapi/TopicController.java
@@ -15,23 +15,33 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
+    // Get All Topic
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
 
+    // Single Topic with id
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
     }
 
+    // Add Topic
     @RequestMapping(method = RequestMethod.POST, value = "/topics")
     public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
     }
 
+    // Update Topic
     @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
     public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
         topicService.updateTopic(id, topic);
+    }
+
+    // Delete Topic
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
     }
 }
